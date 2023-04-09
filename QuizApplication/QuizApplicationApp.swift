d9f2f212d22b7bf529b0af0b06cb4f0f96ab531e
Eduard Caragea <eduardcaragea@gmail.com>
2023-04-09 12:12:13 +0300

    Initial Commit

---
+++ QuizApplication/QuizApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApplicationApp.swift
+//  QuizApplication
+//
+//  Created by Eduard Sorin Caragea on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
